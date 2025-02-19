35771148ebedbe9e78e0887358efcf83be9844e8
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-02-20 00:52:52 +0700

    create db and schema config

--- internal/config/schema.sql
+++ internal/config/schema.sql
@@ -1,16 +1,17 @@
-CREATE TABLE IF NOT EXISTS Users (
-    user_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
+CREATE TABLE users (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP
+    full_name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+DROP TYPE IF EXISTS progress;
 CREATE TYPE progress AS ENUM('Applied', 'Assessment', 'Interview', 'Offering', 'Accepted', 'Withdrew', 'Closed');
 
-CREATE TABLE IF NOT EXISTS Applications (
-    application_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES Users(user_id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS applications (
+    id SERIAL PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     job_position VARCHAR(100) NOT NULL,
     company_name VARCHAR(100) NOT NULL,
     job_location VARCHAR(100) NOT NULL,
@@ -18,33 +19,57 @@ CREATE TABLE IF NOT EXISTS Applications (
     application_progress PROGRESS DEFAULT 'Applied',
     applied_date DATE,
     notes TEXT,
-    created_at TIMESTAMP
-    updated_at TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS Timelines (
-    timeline_id SERIAL PRIMARY KEY,
-    application_id INT NOT NULL REFERENCES Applications(application_id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS timelines (
+    id SERIAL PRIMARY KEY,
+    application_id INT NOT NULL REFERENCES applications(id) ON DELETE CASCADE,
     timeline_name VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS Interviews (
-    interview_id SERIAL PRIMARY KEY,
-    application_id INT NOT NULL REFERENCES Applications(application_id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS interviews (
+    id SERIAL PRIMARY KEY,
+    application_id INT NOT NULL REFERENCES applications(id) ON DELETE CASCADE,
     interview_title VARCHAR(100) NOT NULL,
     interview_date DATE,
     notes TEXT,
-    created_at TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS Contacts (
-    contact_id SERIAL PRIMARY KEY,
-    application_id INT NOT NULL REFERENCES Applications(application_id) ON DELETE CASCADE,
+CREATE TABLE IF NOT EXISTS contacts (
+    id SERIAL PRIMARY KEY,
+    application_id INT NOT NULL REFERENCES applications(id) ON DELETE CASCADE,
     contact_name VARCHAR(100) NOT NULL,
     contact_email VARCHAR(100),
     contact_phone VARCHAR(20),
     position VARCHAR(100),
     notes TEXT,
-    created_at TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS refresh_tokens (
+    token UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS update_users_table_updatedAt ON users;
+CREATE TRIGGER update_users_table_updatedAt
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_modified_column();
